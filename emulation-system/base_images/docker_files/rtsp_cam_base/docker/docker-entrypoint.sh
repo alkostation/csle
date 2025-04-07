821f3de7e5ea02aae216158622d6213acd93e3e7
Marco Campione <per.registrazionifalse@gmail.com>
2025-04-07 15:08:22 +0300

    feat: camera docker updates

--- emulation-system/base_images/docker_files/rtsp_cam_base/docker/docker-entrypoint.sh
+++ emulation-system/base_images/docker_files/rtsp_cam_base/docker/docker-entrypoint.sh
@@ -19,7 +19,51 @@ set -e
 
 # Start the RTSP camera server
 echo "Starting RTSP camera server at port 8554"
-
+# go home:
 cd /root
-# by-pass conda:
-/usr/bin/python3 server.py
+
+# by-pass conda, run at background:
+/usr/bin/python3 server.py &
+
+MY_IP=`ifconfig | sort -r  | grep 'inet ' | head -n 1  | awk '{print $2}' | cut -d':' -f2`
+echo "My IP=$MY_IP Waiting for the ZM server to start."
+
+
+N1=`ifconfig | sort -r  | grep 'inet ' | head -n 1  | awk '{print $2}' | cut -d':' -f2 | sed 's/\./\ /g'| cut -d' ' -f1`
+echo "Nibble 1=$N1"
+
+SRV_IP="$N1.17.2.21"
+
+# DEBUG Override:
+# SRV_IP="172.17.0.3"
+
+echo "Waiting $SRV_IP to start"
+
+# Loop until the target is reachable
+while true; do
+    if ping -c 1 "$SRV_IP" &> /dev/null; then
+        echo "$SRV_IP is up!"
+        break  # Exit the loop if the target is reachable
+    else
+        echo "$SRV_IP is down! Retrying in 5 seconds..."
+        sleep 5  # Wait for 5 seconds before retrying
+    fi
+done
+
+sleep 5 # wait 5s more to start Apache
+
+echo "Trying to register the RTSP cam $MY_IP in hardcoded server $SRV_IP"
+
+while true; do
+    if /usr/bin/python3  rtsp_cam-activate.py "http://$SRV_IP/zm"  "$MY_IP" &> /dev/null; then
+        echo "Cam O.K."
+        break
+    else
+        echo "Cam Failed."
+        sleep 5
+    fi
+done
+
+echo "Entering sleep loop"
+# sleep loop to keep container up
+while true; do sleep 1; done;
